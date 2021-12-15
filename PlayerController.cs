7c11697256a818c966a77ffbba97168931da8be7
Francesco2426 <77923481+Francesco2426@users.noreply.github.com>
2021-12-15 13:41:45 -0500

    added crouch, new bools for running and crouching

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,21 +1,33 @@
-﻿using UnityEngine;
-
+using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public CharacterController cc;
-    [SerializeField] private float walk;
-    [SerializeField] private float run;
+    [SerializeField] private GameObject player;
     [SerializeField] private Camera cam;
     [SerializeField] private float Sensitivity;
-    public bool IsWalking;
+    
+    private float speed;
+    [SerializeField] private float walk;
+    [SerializeField] private float run;
+    [SerializeField] private float crouch;
+
+    private Vector3 crouchScale;
+    private Vector3 normalScale;
+
+    public bool isMoving;
+    public bool isCrouching;
+    public bool isRunning;
+
     private float X;
     private float Y;
 
     private void Start()
     {
+        speed = walk;
+        crouchScale = new Vector3(1, .75f, 1);
+        normalScale = new Vector3(1, 1, 1);
         cc = GetComponent<CharacterController>();
         cc.enabled = true;
-        IsWalking = false;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -30,7 +42,7 @@ public class PlayerController : MonoBehaviour
 
         X += Input.GetAxis("Mouse X") * (Sensitivity * Time.deltaTime);
         Y -= Input.GetAxis("Mouse Y") * (Sensitivity * Time.deltaTime);
-        
+
         if (X < MIN_X)
         {
             X += MAX_X;
@@ -55,25 +67,37 @@ public class PlayerController : MonoBehaviour
         Vector3 forward = transform.forward * vertical;
         Vector3 right = transform.right * horizontal;
 
+        cc.SimpleMove((forward + right) * speed);
         // Determines if the speed = run or walk
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            cc.SimpleMove((forward + right) * run);
+            speed = run;
+            isRunning = true;
+        }
+        //Crouch
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            isCrouching = true;
+            isRunning = false;
+            speed = crouch;
+            player.transform.localScale = crouchScale;
         }
         else
         {
-            cc.SimpleMove((forward + right) * walk);
+            isRunning = false;
+            isCrouching = false;
+            speed = walk;
+            player.transform.localScale = normalScale;
         }
-
         // Detects if the player is moving.
         // Useful if you want footstep sounds and or other features in your game.
         if (cc.velocity.sqrMagnitude > 0.0f)
         {
-            IsWalking = true;
+            isMoving = true;
         }
         else
         {
-            IsWalking = false;
+            isMoving = false;
         }
     }
 }
